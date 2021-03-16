99b78f0bf2d32b662da9224eebcc405ac0cec8cd
Thiano Pereira Lima <thianolima@teltelecom.com.br>
2021-03-15 21:47:06 -0300

    primeiro commit

---
+++ src/main/java/br/com/desafiojavaspringboot/DesafioJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiojavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaSpringbootApplication.class, args);
+	}
+
+}
